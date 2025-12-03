4c48d620bfc742dd8d40c8a73023d09e922e2089
tezifoj <sasha.rozhkova555@gmail.com>
2025-12-03 21:29:04 +0300

    RecordType.java

--- src/enums/RecordType.java
+++ src/enums/RecordType.java
@@ -1,37 +1,18 @@
 package enums;
 
 public enum RecordType {
-    THELITTLEBOOK("книжечка", 50),
-    NOTEBOOK("блокнот", 20),
-    PIECEOFPAPER("лист бумаги", 10);
+    THELITTLEBOOK("книжечка"),
+    NOTEBOOK("блокнот"),
+    PIECEOFPAPER("лист бумаги");
 
     private final String description;
-    private final int capacity;
 
-    RecordType(String description, int capacity) {
+    RecordType(String description) {
         this.description = description;
-        this.capacity = capacity;
     }
 
     @Override
     public String toString() {
         return this.description;
-    }
-
-    public int getCapacity(){
-        return this.capacity;
-    }
-
-    public static RecordType getByCapacity(int capacity){
-        for (RecordType type : values()){
-            if (type.capacity == capacity){
-                return type;
-            }
-        }
-        return null;
-    }
-
-    public boolean hasSpaceFor(int entries) {
-        return entries <= capacity;
     }
 }
