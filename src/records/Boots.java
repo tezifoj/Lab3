5125ccda84ac98813576c0eae68eccb5fefe2375
tezifoj <sasha.rozhkova555@gmail.com>
2025-12-04 10:16:39 +0300

    Update Boots.java

--- src/records/Boots.java
+++ src/records/Boots.java
@@ -1,15 +1,1 @@
-package records;
 
-import interfaces.Lookable;
-
-public record Boots(String name) implements Lookable {
-    @Override
-    public String lookAt() {
-        return "ботинки";
-    }
-
-    @Override
-    public String toString() {
-        return this.lookAt();
-    }
-}
